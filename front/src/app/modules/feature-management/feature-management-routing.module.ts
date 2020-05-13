5199a2a0325da2e3be3c6bb7f59648cd6267440e
Unknown <robin.collas@hotmail.fr>
2020-05-13 15:26:01 +0200

    fix access to feature page when the user is not logged in

--- front/src/app/modules/feature-management/feature-management-routing.module.ts
+++ front/src/app/modules/feature-management/feature-management-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FeaturesPageComponent } from './components/features-page/features-page.component';
+import { LoginActivate } from '../auth/LoginActivateGuard';
 
-const featuresRoutes: Routes = [{ path: 'feature', component: FeaturesPageComponent }];
+const featuresRoutes: Routes = [{ path: 'feature', component: FeaturesPageComponent, canActivate: [LoginActivate] }];
 
 @NgModule({
 	imports: [RouterModule.forChild(featuresRoutes)],
-	exports: [RouterModule]
+	exports: [RouterModule],
+  providers: [LoginActivate]
 })
 export class FeatureManagementRoutingModule {}
